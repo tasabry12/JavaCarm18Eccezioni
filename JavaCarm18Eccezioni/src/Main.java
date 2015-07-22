872f468632cb923aec316be792f6fdb0a0f297ec
v-matoscano <v-matoscano@ROMR90F84D0.SEA.CORP.EXPECN.com>
2015-07-22 17:38:44 +0200

    Initial commit

---
+++ JavaCarm18Eccezioni/src/Main.java
@@ -0,0 +1,25 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+
+/*
+ * import java.lang.Throwable   <--  Super classe delle eccezioni
+ * 
+ * Ha 2 sottoclassi
+ * 1) Exception:  flusso si esecuzione imprevisto, gestibile
+ * 2) Error:  flusso si esecuzione imprevisto, non gestibile (es. teminazione risorse di memoria)
+ * 
+ * Poi abbiamo
+ * Asserzioni: sono istruzioni che se verificate garantiscono la correttezza di un blocco di codice (assert), si 
+ * implementano tramite assert
+ * 
+ * Per le eccezioni possiamo fare questa distinzione
+ * Checked Exception: try-catch (sono quelle che il compilatore ci obbliga a gestire)
+ * Unchecked Exception: RuntimeException (es NullPointerException, ArrayIndexOutOfBounds)
+ *                      (sono quelle non controllabili perchè avvengono a run time)
+ */
+
+	}
+
+}
